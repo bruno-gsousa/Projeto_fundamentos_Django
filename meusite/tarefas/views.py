40d4de5f101c13373dfa57d058486693e1601dc7
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-07-15 19:20:04 -0300

    crud_final

--- meusite/tarefas/views.py
+++ meusite/tarefas/views.py
@@ -26,3 +26,17 @@ def tarefas_remover(request,id):
     tarefa = get_object_or_404(TarefaModel,id=id)
     tarefa.delete()
     return redirect("tarefas:home")
+
+def tarefas_editar(request,id):
+    tarefa = get_object_or_404(TarefaModel,id=id)
+    if request.method == "POST":
+        formulario = TarefaForm(request.POST,instance=tarefa)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect("tarefas:home")
+    else:
+        contexto={
+            "form":TarefaForm(instance=tarefa)
+        }
+    
+    return render(request, "pagetarefas/editar.html",contexto)
